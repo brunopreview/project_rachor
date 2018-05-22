cece2bd01980ab6caba471e39543967346d4f252
Bruno Henrique <brunohs.dev@gmail.com>
2018-05-22 14:33:50 -0300

    ajustes e testes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		// echo Stringy\Stringy::create('teste teste teste')->camelize();
-		$this->load->view('welcome_message');
+		echo Stringy\Stringy::create('teste teste teste')->camelize();
+		// $this->load->view('welcome_message');
 	}
 }
